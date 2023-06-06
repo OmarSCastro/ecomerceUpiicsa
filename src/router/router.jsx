244568654ea7f5aa1c16b9fb76f5b203848efdff
TITO30 <moralestito569@gmail.com>
2023-06-06 12:40:20 -0600

    Add the router and components folder

---
+++ src/router/router.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+
+export const routes = [
+  { path: "/", element: <App /> },
+];
+
+export const router = createBrowserRouter(routes);
